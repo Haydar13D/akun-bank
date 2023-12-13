23912d2cd8af928715177cee836fa9d2542c5179
Haydar13D <garoxjeber@gmail.com>
2023-12-13 21:26:16 +0700

    perubahan total

--- try.py
+++ try.py
@@ -1,75 +1,64 @@
 import hashlib
 
-class akun_bank:
+class AkunBank:
     def __init__(self, username, password):
         self.username = username
         self.password = self._hash_password(password)
 
     def _hash_password(self, password):
-        # Menggunakan hashlib untuk membuat hash SHA-256 dari kata sandi
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
         return hashed_password
+
     def _encrypt_password(self, password):
-        # Fungsi ini dapat digunakan untuk menghasilkan kode enkripsi dari password
-        # Sesuaikan dengan algoritma enkripsi yang Anda inginkan
         encrypted_password = hashlib.sha256(password.encode()).hexdigest()
         return encrypted_password
 
     def get_encrypted_password(self):
-        # Mengembalikan kode enkripsi dari password yang sudah dipanggil
         return self._encrypt_password(self.password)
 
-
     def check_password(self, input_password, input_username):
-        # Memeriksa apakah kata sandi yang dimasukkan cocok dengan yang disimpan
         return self.username == input_username and self.password == self._hash_password(input_password)
 
-def read_data_rahasia_from_file(data_rahasia):
+def read_data_rahasia_from_file():
     try:
         with open("data_rahasia.txt", 'r') as file:
             lines = file.readlines()
 
-            if len(lines) >= 2:
-                username = lines[0].strip()
-                password = lines[1].strip()
-                return username, password 
-            else:
-                print("File tidak memenuhi struktur yang diharapkan.")
-                return None, None
-    except FileNotFoundError:
-        print(f"File {data_rahasia} not found.")
-        return None, None
+            accounts = []
+            i = 0
+            while i < len(lines):
+                username = lines[i].strip()
+                password = lines[i + 1].strip()
+                account = AkunBank(username, password)
+                accounts.append(account)
+                i += 2
 
+            return accounts
+    except FileNotFoundError:
+        print("File data_rahasia.txt not found.")
+        return None
 
 def main():
-    # Mengambil data dari berkas
-    filename = 'data_rahasia.txt'
-    username, password = read_data_rahasia_from_file('data_rahasia.txt')
+    accounts = read_data_rahasia_from_file()
 
-    if username and password:
-        # Membuat akun
-        user_account = akun_bank(username, password)
-        print("Akun berhasil dibuat.")
-        encrypted_password = user_account.get_encrypted_password()
-        
-        print(f"Kode enkripsi dari password: {encrypted_password}")
+    if accounts:
+        for user_account in accounts:
+            print(f"Akun berhasil dibuat untuk {user_account.username}.")
+            encrypted_password = user_account.get_encrypted_password()
+            print(f"Kode enkripsi dari password: {encrypted_password}")
 
-        # Menggunakan akun
-        for n in range(3):
-            login_attempt = str(input("Masukkan username untuk masuk: "))
-            login_attempt1 = str(input("Masukkan kata sandi untuk masuk: "))
+            for n in range(3):
+                login_attempt = str(input("Masukkan username untuk masuk: "))
+                login_attempt1 = str(input("Masukkan kata sandi untuk masuk: "))
 
-            if user_account.username == login_attempt and user_account.check_password (login_attempt1,login_attempt):
-                print("Masuk berhasil.")
-                break
-            else:
-                print("Kata sandi salah. Masuk gagal.")
-                if n == 2:
-                    print("anda telah mencoba 3 kali")
-                    break
-                
+                if user_account.username == login_attempt and user_account.check_password(login_attempt1, login_attempt):
+                    print("Masuk berhasil.")
+                    return
+                else:
+                    print("Kata sandi salah. Masuk gagal.")
+                    if n == 2:
+                        print("Anda telah mencoba 3 kali.")
+                        break
 
 if __name__ == "__main__":
     main()
-#hisdh9shd9wfsdssdsds
-#dsindsjdi
